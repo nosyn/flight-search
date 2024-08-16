1302050e5686f085a55524332dad2a688df2e95b
Son Nguyen <biem97@gmail.com>
2024-08-16 08:12:01 +0700

    Set up cors and morgan

--- backend/src/constant.ts
+++ backend/src/constant.ts
@@ -1,1 +1,5 @@
-export const PORT = 3000;
+export const __DEV__ = process.env.NODE_ENV === 'development';
+
+export const HOST = process.env.HOST || 'localhost';
+export const PORT = parseInt(process.env.PORT || '8000');
+export const PROD_CORS_ORIGIN = process.env.PROD_CORS_ORIGIN || '';
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,15 +1,40 @@
 import express from 'express';
-import { PORT } from './constant';
+import { __DEV__, HOST, PORT, PROD_CORS_ORIGIN } from './constant';
+import morgan from 'morgan';
+import cors from 'cors';
 
 const main = () => {
   const app = express();
+
+  // Middlewares
+  app.use(
+    express.json({
+      limit: '10mb',
+    })
+  );
+  app.use(morgan(__DEV__ ? 'dev' : 'combined'));
+
+  if (__DEV__) {
+    console.warn('Running in development mode - allowing CORS for all origins');
+    app.use(cors());
+  } else if (PROD_CORS_ORIGIN) {
+    console.log(
+      `Running in production mode - allowing CORS for domain: ${PROD_CORS_ORIGIN}`
+    );
+    const corsOptions = {
+      origin: PROD_CORS_ORIGIN, // Restrict to production domain
+    };
+    app.use(cors(corsOptions));
+  } else {
+    console.warn('Production CORS origin not set, defaulting to no CORS.');
+  }
 
   app.get('/', (_, res) => {
     res.send('Welcome to flight search');
   });
 
-  app.listen(PORT, () => {
-    console.log('Server is up and ready 🚀🚀🚀');
+  app.listen(PORT, HOST, () => {
+    console.log(`Server is up and ready at ${HOST}:${PORT} 🚀🚀🚀`);
   });
 };
 

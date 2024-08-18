7b1a5d5a512162fc77f3f00c1ad7b93ff647e5ee
Son Nguyen <biem97@gmail.com>
2024-08-18 17:33:11 +0700

    Add timestamp boundary for flight searching

--- backend/src/controllers/flights.controller.ts
+++ backend/src/controllers/flights.controller.ts
@@ -1,8 +1,9 @@
+import { and, eq, gte, lt } from 'drizzle-orm';
 import { Request, Response } from 'express';
-import { db } from '../libs/db';
 import { z } from 'zod';
-import { and, eq } from 'drizzle-orm';
+import { db } from '../libs/db';
 import { flightsScheduleTable } from '../libs/db/schema';
+import { getDateTimestampFromQuery } from '../libs/utils';
 
 const getFlightQuery = z.object({
   origin: z.string(),
@@ -23,15 +24,19 @@ export const getFlights = async (req: Request, res: Response) => {
       return;
     }
 
+    const queryTimestamp = getDateTimestampFromQuery(data.dateFrom);
     const airports = await db.query.flightsScheduleTable.findMany({
       where: and(
         eq(flightsScheduleTable.departureAirport, data.origin),
-        eq(flightsScheduleTable.arrivalAirport, data.destination)
+        eq(flightsScheduleTable.arrivalAirport, data.destination),
+        gte(flightsScheduleTable.departureTime, queryTimestamp.start),
+        lt(flightsScheduleTable.departureTime, queryTimestamp.end)
       ),
     });
 
     res.status(200).json(airports);
   } catch (error) {
+    console.error('Error while getting flights: ', error);
     return res.status(500).json({ message: 'Internal server error' });
   }
 };
--- backend/src/libs/utils.ts
+++ backend/src/libs/utils.ts
@@ -1,2 +1,10 @@
 export const sleep = (ms: number = 1000) =>
   new Promise((resolve) => setTimeout(resolve, ms));
+
+export const getDateTimestampFromQuery = (date: string) => {
+  const [day, month, year] = date.split('-').map(Number);
+  return {
+    start: new Date(year, month - 1, day, 0, 0, 0, 0),
+    end: new Date(year, month - 1, day + 1, 0, 0, 0, 0),
+  };
+};
